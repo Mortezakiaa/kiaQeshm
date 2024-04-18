87d8f482a2e3dc51624ce15209aa9d71ce036bd0
mortezaKia <morteza.kia8700@gmail.com>
2024-04-18 12:47:22 +0330

    some changes on table

--- src/components/EditableTable.tsx
+++ src/components/EditableTable.tsx
@@ -1,65 +1,94 @@
-'use client'
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import { TextField } from '@mui/material';
-import { useState } from 'react';
+"use client";
+import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import { Tooltip } from "@mui/material";
+import { useState } from "react";
+import CancelIcon from "@mui/icons-material/Cancel";
+import CheckIcon from "@mui/icons-material/Check";
 
-function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-) {
-  return { name, calories, fat, carbs, protein};
-}
-
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0 ),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-];
-
-
-export default function EditableTable() {
-    const [editMode , setEditMode] = useState(false)
-    const fn = (e:any)=>{
-        setEditMode(true)
-    }
+export default function StickyHeadTable() {
+  const [editMode, setEditMode] = useState(false);
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="caption table">
-        <caption>A basic table example with a caption</caption>
-        <TableHead>
-          <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell onClick={fn} align="right">
-                {editMode ? <TextField defaultValue={row.calories}/>:row.calories}
-               </TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+    <Paper sx={{ width: "100%", overflow: "hidden" }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader sx={{
+          '& th':{
+            backgroundColor:'#424242',
+            color:'white'
+          }
+        }}>
+          <TableHead>
+            <TableRow>
+              <TableCell align="right">*</TableCell>
+              <TableCell align="right">نام کالا</TableCell>
+              <TableCell align="right">تعداد</TableCell>
+              <TableCell align="right">فی</TableCell>
+              <TableCell align="right">قیمت قبل تخفیف</TableCell>
+              <TableCell align="right">درصد تخفیف</TableCell>
+              <TableCell align="right">قیمت تخفیف</TableCell>
+              <TableCell align="right">قیمت نهایی</TableCell>
+              <TableCell align="right">توضیحات</TableCell>
+              <TableCell align="right">وضعیت</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody  sx={{'& tr:nth-child(even)':{
+              backgroundColor:'#e0e0e0'
+            }}}>
+            <TableRow>
+              <TableCell align="right">1</TableCell>
+              <TableCell align="right">d</TableCell>
+              <TableCell align="right">das</TableCell>
+              <TableCell align="right">das</TableCell>
+              <TableCell align="right">da</TableCell>
+              <TableCell align="right">das</TableCell>
+              <TableCell align="right">da</TableCell>
+              <TableCell align="right">da</TableCell>
+              <TableCell align="right">da</TableCell>
+              <TableCell align="right">
+                {!editMode ? (
+                  <>
+                    <Tooltip title="حذف">
+                      <IconButton color="error">
+                        <DeleteIcon />
+                      </IconButton>
+                    </Tooltip>
+                    <Tooltip title="ویرایش">
+                      <IconButton
+                        color="success"
+                        onClick={() => setEditMode(true)}
+                      >
+                        <EditIcon />
+                      </IconButton>
+                    </Tooltip>
+                  </>
+                ) : null}
+                {editMode ? (
+                  <>
+                    <Tooltip title="انصراف">
+                      <IconButton color="error" onClick={() => setEditMode(false)}>
+                        <CancelIcon />
+                      </IconButton>
+                    </Tooltip>
+                    <Tooltip title="تایید">
+                      <IconButton color="success">
+                        <CheckIcon />
+                      </IconButton>
+                    </Tooltip>
+                  </>
+                ) : null}
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Paper>
   );
 }
