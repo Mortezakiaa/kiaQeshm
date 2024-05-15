903202786ef05392dcd737caf288d21bc534417f
morteza kia <morteza.kia8700@gmail.com>
2024-05-15 11:13:41 +0330

    finaly jest satisfies

---
+++ src/__tests__/Order.test.tsx
@@ -0,0 +1,11 @@
+import '@testing-library/jest-dom'
+import Page from "@/app/Login/page";
+import { render } from "@testing-library/react";
+
+
+describe("", () => {
+  test('' , ()=>{
+    
+
+  })
+});
--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -2,7 +2,7 @@ import { getCookie } from "@/actions/getCookie";
 import Login from "@/forms/Login";
 import { redirect } from "next/navigation";
 
-export default async function page() {
+export default async function Page() {
   const isExistCookie = await getCookie()
   if(isExistCookie)redirect('/Order')
   
