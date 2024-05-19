4c11e2e0dbde045182947065ff923bb0138eb0c4
mortezaKia <morteza.kia8700@gmail.com>
2024-05-19 16:51:56 +0330

    add sample

---
+++ src/utils/Debounce.ts
@@ -0,0 +1,25 @@
+//trailing debounce is the concept for example when write text in input and send request to backend
+// when time is over request sended (just only send one request and to consider last request)
+// first argument get onchange function and secound get time
+export function trailingDebounce(func: any, t: number | string) {
+  let timer: any;
+  return (...args: any) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, t as any);
+  };
+}
+
+// leading debounce is when send request to backend leading concept suppose first request and
+// remove next requests
+export function leadingDebounce(func: any, t: string | number) {
+  let timer: any;
+  return (...args: any) => {
+    if (!timer) {
+      func.apply(this, args);
+    }
+    clearTimeout(timer);
+    timer = setTimeout(() => (timer = undefined), t as any);
+  };
+}
